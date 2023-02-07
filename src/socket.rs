b07df44c36bccd5eb55bc52a847f588b2146bdc1
suba <84484832+suba327777@users.noreply.github.com>
2023-02-07 15:30:44 +0900

    add : socket connection status

--- src/socket.rs
+++ src/socket.rs
@@ -3,6 +3,7 @@ use crate::tcpflags;
 use anyhow::{Context, Ok, Result};
 use pnet::packet::{ip::IpNextHeaderProtocols, Packet};
 use pnet::transport::{self, TransportChannelType, TransportProtocol, TransportSender};
+use pnet::util;
 use std::collections::VecDeque;
 use std::fmt::{self, Display};
 use std::net::{IpAddr, Ipv4Addr};
@@ -19,10 +20,56 @@ pub struct Socket {
     pub remote_addr: Ipv4Addr,
     pub local_port: u16,
     pub remote_port: u16,
+    pub send_param: SendParam,
+    pub recv_param: RecvParam,
+    pub status: TcpStatus,
     pub sender: TransportSender,
 }
 
-pub enum TcpStatus {}
+#[derive(Clone, Debug)]
+pub struct SendParam {
+    pub unacked_seq: u32, //送信後ackされていないseqの先頭
+    pub next: u32,        //次の送信
+    pub window: u16,      //送信ウィンドウサイズ
+    pub initial_seq: u32, //初期送信seq
+}
+
+#[derive(Clone, Debug)]
+pub struct RecvParam {
+    pub next: u32,        //次受信するseq
+    pub window: u16,      //受信ウィンドウ
+    pub initial_seq: u32, //初期受信seq
+    pub tail: u32,        //受信seqの最後尾
+}
+
+pub enum TcpStatus {
+    Listen,
+    SynSent,
+    SynRcvd,
+    Established,
+    FinWait1,
+    FinWait2,
+    TimeWait,
+    CloseWait,
+    LastAck,
+}
+
+impl Display for TcpStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TcpStatus::Listen => write!(f, "LISTEN"),
+            TcpStatus::SynSent => write!(f, "SYNSENT"),
+            TcpStatus::SynRcvd => write!(f, "SYNRCVD"),
+            TcpStatus::SynRcvd => write!(f, "SYNRCVD"),
+            TcpStatus::Established => write!(f, "ESTABLISHED"),
+            TcpStatus::FinWait1 => write!(f, "FINWAIT1"),
+            TcpStatus::FinWait2 => write!(f, "FINWAIT2"),
+            TcpStatus::TimeWait => write!(f, "TIMEWAIT"),
+            TcpStatus::CloseWait => write!(f, "CLOSEWAIT"),
+            TcpStatus::LastAck => write!(f, "LASTACK"),
+        }
+    }
+}
 
 impl Socket {
     pub fn new(
@@ -30,6 +77,7 @@ impl Socket {
         remote_addr: Ipv4Addr,
         local_port: u16,
         remote_port: u16,
+        status: TcpStatus,
     ) -> Result<Self> {
         //送信用のチャネルを使用し、セグメントの送信を行う
         let (sender, _) = transport::transport_channel(
@@ -42,20 +90,54 @@ impl Socket {
             remote_addr,
             local_port,
             remote_port,
+            send_param: SendParam {
+                unacked_seq: 0,
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+            },
+            recv_param: RecvParam {
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+                tail: 0,
+            },
+            status,
             sender,
         })
     }
 
-    pub fn send_tcp_packet(&mut self, flag: u8, payload: &[u8]) -> Result<usize> {
+    pub fn send_tcp_packet(
+        &mut self,
+        seq: u32,
+        ack: u32,
+        flag: u8,
+        payload: &[u8],
+    ) -> Result<usize> {
         let mut tcp_packet = TCPPacket::new(payload.len());
         tcp_packet.set_src(self.local_port);
         tcp_packet.set_dest(self.local_port);
+        tcp_packet.set_seq(seq);
+        tcp_packet.set_ack(ack);
+        tcp_packet.set_data_offset(5); //使わないので固定
         tcp_packet.set_flag(flag);
+        tcp_packet.set_window_size(self.recv_param.window);
+        tcp_packet.set_payload(payload);
+        tcp_packet.set_checksum(util::ipv4_checksum(
+            &tcp_packet.packet(),
+            8,
+            &[],
+            &self.local_addr,
+            &self.remote_addr,
+            IpNextHeaderProtocols::Tcp,
+        ));
 
         let sent_size = self
             .sender
             .send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr))
-            .unwrap();
+            .context(format!("failed to send: \n{:?}", tcp_packet))?;
+
+        dbg!("sent", &tcp_packet);
         Ok(sent_size)
     }
 
